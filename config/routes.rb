0540ee6dbca5a2324359dd9e13c8b97992fc0355
Matthew Davenport <davenmport@gmail.com>
2022-06-30 15:35:19 -0700

    database for users added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   get 'about', to: 'pages#about'     #use about_path
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
